22eb68a8fbd918a713c4fc2382fd6c16cd7a8b96
Andrew Zaichenko <zaichenkoand@gmail.com>
2023-07-26 23:04:14 +0300

    File hw03.py was added.

---
+++ hw03.py
@@ -0,0 +1,15 @@
+"""
+Порахувати кількість унікальних символів в строці.
+Якщо їх більше 10 - вивести в консоль True, інакше - False.
+Строку отримати за допомогою функції input()
+"""
+
+user_string = input("Please enter a sentence to discover the quantity of unique symbols -> ").replace(" ", "").lower()
+print(user_string)
+user_set = set(user_string)
+print(user_set)
+
+if len(user_set) > 10:
+    print(f"That's {True}, {len(user_set)} is greater than 10.")
+else:
+    print(f"{False}, {len(user_set)} is less than 10.")
