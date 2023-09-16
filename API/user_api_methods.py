addd9df7fe88b5ff6532ad0111586810382b8ef3
Andrii Zaichenko <zaichenkoand@gmail.com>
2023-09-16 13:45:45 +0300

    API folder update

--- API/user_api_methods.py
+++ API/user_api_methods.py
@@ -33,7 +33,7 @@ user_session = requests.session()
 def user_signup_method():
     user_signup_data = UserSignUpModel("Lawrence", "Muggerud", "cypress@hill.com", "Qwerty12345", "Qwerty12345")
     user_signup = requests.post(url="https://qauto2.forstudy.space/api/auth/signup", json=user_signup_data.__dict__)
-    print(user_signup, user_signup.text)
+    # print(user_signup, user_signup.text)
     return user_signup
 
 
@@ -41,19 +41,19 @@ def user_signup_method():
 def user_signin_method():
     user_signin_data = UserSignInModel("cypress@hill.com", "Qwerty12345")
     user_signin = user_session.post(url="https://qauto2.forstudy.space/api/auth/signin", json=user_signin_data.__dict__)
-    print(user_signin, user_signin.text)
+    # print(user_signin, user_signin.text)
     return user_signin
 
 
 # Check a User Profile (Gets authenticated user profile data)
 def get_user_profile_data_method():
     get_user_profile_data = user_session.get(url="https://qauto2.forstudy.space/api/users/profile")
-    print(get_user_profile_data, get_user_profile_data.text)
+    # print(get_user_profile_data, get_user_profile_data.text)
     return get_user_profile_data
 
 
 # Deletes user's account and current user session
 def delete_user_account_method():
     delete_user_account = user_session.delete(url="https://qauto2.forstudy.space/api/users")
-    print(delete_user_account, delete_user_account.text)
+    # print(delete_user_account, delete_user_account.text)
     return delete_user_account
